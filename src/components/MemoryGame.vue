7f3d52f50103fd201cfcdef3fc3bc8819ca70bea
vo <vovasso550@rambler.ru>
2023-04-19 23:57:16 +0300

    main-page

--- src/components/MemoryGame.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-</script>
-
-<template>
- <div>memory component</div>
-</template>
--- src/views/HomeView.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-import MemoryGame from '../components/MemoryGame.vue'
-</script>
-
-<template>
-  <main>
-    <memory-game />
-  </main>
-</template>
