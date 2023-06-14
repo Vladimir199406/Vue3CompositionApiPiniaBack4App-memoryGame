5173c655e940773e2867618c8a74f5c40b8685d4
vo <vovasso550@rambler.ru>
2023-06-14 18:58:29 +0300

    minor-ui-bug-fix

--- src/components/Auth/RegisterForm.vue
+++ src/components/Auth/RegisterForm.vue
@@ -72,6 +72,7 @@
         type="submit"
         :disabled="reg_in_submission"
         class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition hover:bg-purple-700"
+        style="width: 200px; margin: auto"
       >
         Submit
       </button>
